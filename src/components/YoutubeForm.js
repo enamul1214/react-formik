04484c4add5ebbc734d8a6dc91a920926b693fe9
enamul kabir <enamul.kabirseu@gmail.com>
2021-02-27 13:33:46 +0600

    form validation added

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,17 +1,47 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import './YoutubeForm.css'
+
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+
+const onSubmit = values => {
+    console.log('form data:', values);
+}
+
+const validate = values => {
+    // values.name values.email values.channel
+    // errors.name errors.email errors.channel
+    // errors.name = 'This field is required
+    let errors = {}
+
+    if(!values.name){
+        errors.name = 'This name field is required';
+    }
+
+    if(!values.email){
+        errors.email = 'This email field is required';
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+        errors.email = 'Invalid email address';
+    }
+
+    if(!values.channel){
+        errors.channel = 'This channel field is required';
+    }
+
+    return errors;
+}
 
 function YoutubeForm() {
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-
-        onSubmit: values => {
-            console.log('form data:', values);
-        }
+        initialValues,
+        onSubmit,
+        validate
     });
 
     // console.log('form values:', formik.values);
@@ -27,8 +57,10 @@ function YoutubeForm() {
                         id = "name"
                         name = "name"
                         onChange = { formik.handleChange }
+                        onBlur= {formik.handleBlur}
                         value = { formik.values.name }
                     />
+                    {formik.touched.name && formik.errors.name ? (<div className="error">{formik.errors.name}</div>) : null}
                 </div>
 
                 <div className = "form-group" >
@@ -39,8 +71,10 @@ function YoutubeForm() {
                         id = "email"
                         name = "email"
                         onChange = { formik.handleChange }
+                        onBlur= {formik.handleBlur}
                         value = { formik.values.email }
-                    /> 
+                    />
+                    {formik.touched.email && formik.errors.email ? (<div className="error">{formik.errors.email}</div>) : null}
                 </div>
 
                 <div className = "form-group" >
@@ -51,8 +85,10 @@ function YoutubeForm() {
                         id = "channel"
                         name = "channel"
                         onChange = { formik.handleChange }
+                        onBlur= {formik.handleBlur}
                         value = { formik.values.channel }
                     />
+                    {formik.touched.channel && formik.errors.channel ? (<div className="error">{formik.errors.channel}</div>) : null}
                 </div>
 
                 <button className="btn btn-primary" type="submit">Submit</button> 
